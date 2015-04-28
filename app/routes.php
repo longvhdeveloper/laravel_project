a658cfe889a7c0dd2cd972b703313a499bf6762e
Long <longvo@Longs-MacBook-Pro.local>
2015-04-28 09:08:16 +0700

    setup sentry 2 for laravel4

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,19 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::get('create_user', function(){
+    $user = Sentry::getUserProvider()->create(
+        array(
+            'email' => 'vohoanglong07@gmail.com',
+            'password' => '2871989',
+            'username' => 'jackie',
+            'first_name' => 'Jackie',
+            'last_name' => 'Wu',
+            'activated' => '1',
+            'permissions' => array(
+                'admin' => 1
+            )
+    ));
 
-Route::get('/', function()
-{
-	return View::make('hello');
+    return 'Done';
 });
